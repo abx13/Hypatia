de6ecb0be9efaf8bf0c03a7329a221204cd9739c
kalelpida <cordip@outlook.fr>
2022-03-18 01:37:43 +0100

    plus besoin de git submodule update

--- hypatia_install_dependencies.sh
+++ hypatia_install_dependencies.sh
@@ -24,7 +24,7 @@ echo "Installing dependencies for ns3-sat-sim..."
 sudo apt-get -y install openmpi-bin openmpi-common openmpi-doc libopenmpi-dev lcov gnuplot || exit 1
 pip install numpy statsmodels || exit 1
 pip install git+https://github.com/snkas/exputilpy.git@v1.6 || exit 1
-git submodule update --init --recursive || exit 1
+#git submodule update --init --recursive || exit 1 #no more submodules. erroneous command, basic-sim module must have an older HEAD. 
 
 # satviz
 echo "There are currently no dependencies for satviz."
